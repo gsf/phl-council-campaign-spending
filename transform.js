9042b9e395b6e76e4ecded2116d234a9e05a5891
Gabriel Farrell <g@grrawr.com>
2013-12-12 00:06:32 -0500

    Added data transformation script. Closes #2

---
+++ transform.js
@@ -0,0 +1,41 @@
+var csv = require('csv');
+var fs = require('fs');
+
+// base32 according to http://www.crockford.com/wrmg/base32.html
+var alphabet = '0123456789abcdefghjkmnpqrstvwxyz';
+function generateId () {
+  var id = '';
+  for (var i=0; i<4; i++) {
+    id += alphabet[Math.floor(Math.random() * alphabet.length)];
+  }
+  return id;
+}
+
+csv()
+.from.stream(fs.createReadStream(__dirname+'/campaign-spending.csv'), {columns: true, trim: true})
+.to.path(__dirname+'/campaign-spending.out')
+.transform(function (row) {
+  process.stdout.write('.');
+  return JSON.stringify({
+    index: {
+      _index: 'campaign-spending',
+      _type: 'record',
+      _id: generateId()
+    }
+  }) + '\n' +
+  JSON.stringify({
+    name: row.Councilperson,
+    payee: row.Payee,
+    address: row['Payee Address'],
+    date: row['Date'],
+    amount: row.Amount,
+    description: row.Description,
+    category: row.Category
+  }) + '\n';
+})
+.on('end', function (count) {
+  console.log(count);
+})
+.on('error', function (err) {
+  console.error(err.stack);
+});
